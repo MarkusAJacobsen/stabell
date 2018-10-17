67320c98bec45e686d428bbb7ddfe392227d0caa
MarkusJacobsen <markusja@stud.ntnu.no>
2018-10-17 21:55:20 +0200

    Styling, WIP save current tabs

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
 all: ensure_format lint transpile
 
+no_dev_check: transpile
+
 ensure_format:
 	gofmt -s -w .
 
--- stabell.go
+++ stabell.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/fabioberger/chrome"
 	"honnef.co/go/js/dom"
-	"strconv"
 )
 
 func main() {
-	c := chrome.NewChrome()
-
-	tabDetails := chrome.Object{
-		"active": false,
+	session := Session{
+		chrome.NewChrome(),
 	}
 
-	c.Tabs.Create(tabDetails, func(tab chrome.Tab) {
-		notification := "Tab with id: " + strconv.Itoa(tab.Id) + " created!"
-		dom.GetWindow().Document().GetElementByID("notification").SetInnerHTML(notification)
-	})
+	el1 := dom.GetWindow().Document().GetElementByID("vm-1")
+	el1.AddEventListener("click", true, session.SaveWindowSession)
+}
+
+type Session struct {
+	chrome *chrome.Chrome
+}
+
+// Save current window tabs to storage
+func (session Session) SaveWindowSession(event dom.Event) {
+	fmt.Println("Triggered with event: ", event)
+	// Get tabs in current window
+
+	queryInfo := chrome.Object{
+		"currentWindow": true,
+	}
+
+	session.chrome.Tabs.Query(queryInfo, session.saveTabs)
+}
+
+func (session Session) saveTabs(tabs []chrome.Tab) {
+	/*urls*/_, err := generateJSON(tabs)
+	if err != nil {
+		//
+	}
+
+	/*fmt.Println(session.chrome.Storage.Sync.Get("urls"))
+	session.chrome.Storage.Sync.Set("urls", string(urls))
+	fmt.Println(session.chrome.Storage.Sync.Get("urls"))*/
+}
+
+func generateJSON(tabs []chrome.Tab) (mu []byte, err error) {
+	var urls = make(map[int]string)
+
+	for i := range tabs {
+		urls[i] = tabs[i].Url
+	}
+
+	mu, err = json.Marshal(urls)
+
+	return
 }
