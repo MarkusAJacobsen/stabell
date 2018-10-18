076c469c11d991796d08b45cf64f42af74fe718f
MarkusJacobsen <markusja@stud.ntnu.no>
2018-10-18 02:59:10 +0200

    Save with restore, and get works (No processing).

--- stabell.go
+++ stabell.go
@@ -4,53 +4,146 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/fabioberger/chrome"
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/pkg/errors"
 	"honnef.co/go/js/dom"
+	"time"
+)
+
+const (
+	StorageKey  string = "stabell-storage"
+	JSUndefined string = "undefined"
+)
+
+var (
+	cStorage = js.Global.Get("localStorage")
 )
 
 func main() {
-	session := Session{
+	ctx := Ctx{
 		chrome.NewChrome(),
+		dom.GetWindow().Document().GetElementByID("status"),
 	}
 
 	el1 := dom.GetWindow().Document().GetElementByID("vm-1")
-	el1.AddEventListener("click", true, session.SaveWindowSession)
+	el1.AddEventListener("click", true, ctx.SaveWindowSession)
+
+	el2 := dom.GetWindow().Document().GetElementByID("vm-2")
+	el2.AddEventListener("click", true, GetSavedSessionsHandler)
 }
+
+type Ctx struct {
+	chrome *chrome.Chrome
+	status dom.Element
+}
+
+type Storage []Session
 
 type Session struct {
-	chrome *chrome.Chrome
+	Ts  int64    `json:"ts"`
+	Url []string `json:"url"`
 }
 
-// Save current window tabs to storage
-func (session Session) SaveWindowSession(event dom.Event) {
-	fmt.Println("Triggered with event: ", event)
-	// Get tabs in current window
+func (ctx Ctx) SetStatus(status string) {
+	now := time.Now().Format("01/02 15:04:05")
+	ctx.status.SetInnerHTML(status + " " + now)
+}
 
+// Save current window tabs to cStorage
+func (ctx Ctx) SaveWindowSession(event dom.Event) {
 	queryInfo := chrome.Object{
 		"currentWindow": true,
 	}
 
-	session.chrome.Tabs.Query(queryInfo, session.saveTabs)
+	ctx.chrome.Tabs.Query(queryInfo, ctx.saveTabs)
 }
 
-func (session Session) saveTabs(tabs []chrome.Tab) {
-	/*urls*/_, err := generateJSON(tabs)
-	if err != nil {
-		//
+func (ctx Ctx) saveTabs(tabs []chrome.Tab) {
+	urls := generateJSON(tabs)
+
+	session := Session{
+		time.Now().Unix(),
+		mapToStringArray(urls),
 	}
 
-	/*fmt.Println(session.chrome.Storage.Sync.Get("urls"))
-	session.chrome.Storage.Sync.Set("urls", string(urls))
-	fmt.Println(session.chrome.Storage.Sync.Get("urls"))*/
+	restore, err := getFromChromeStorage()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if isSame(restore, session) {
+		ctx.SetStatus("Session already saved")
+		return
+	}
+
+	storage := append(restore, session)
+
+	ctx.saveToChromeStorage(storage)
 }
 
-func generateJSON(tabs []chrome.Tab) (mu []byte, err error) {
-	var urls = make(map[int]string)
+func (ctx Ctx) saveToChromeStorage(storage Storage) (err error) {
+	bytes, err := json.Marshal(storage)
+	if err != nil {
+		return
+	}
+
+	cStorage.Set(StorageKey, string(bytes))
+
+	ctx.SetStatus("Session saved")
+	return
+}
+
+func getFromChromeStorage() (storage Storage, err error) {
+	res := cStorage.Get(StorageKey).String()
+	storage = Storage{}
+
+	if res != JSUndefined {
+		err = json.Unmarshal([]byte(res), &storage)
+		if err != nil {
+			errors.Wrap(err, "Thrown in getFromChromeStorage")
+			return
+		}
+	}
+	return
+}
+
+func generateJSON(tabs []chrome.Tab) (urls map[int]string) {
+	urls = make(map[int]string)
 
 	for i := range tabs {
 		urls[i] = tabs[i].Url
 	}
 
-	mu, err = json.Marshal(urls)
+	return
+}
+
+func mapToStringArray(val map[int]string) (con []string) {
+	con = make([]string, 0, len(val))
+
+	for _, value := range val {
+		con = append(con, value)
+	}
 
 	return
+}
+
+func GetSavedSessionsHandler(event dom.Event) {
+	res, err := getFromChromeStorage()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(res)
+}
+
+func isSame(storage Storage, session Session) (same bool) {
+	/*for i := range storage {
+		for j := range storage[i].Url {
+			if !strings.EqualFold(storage[i].Url[j], session.Url[j]) {
+				continue
+			}
+		}
+	}*/
+	return false
 }
